97eeb62a486add62bde4d47f0624b02c203ad8b8
anttijuo <anttijuo@gmail.com>
2024-05-04 10:07:28 +0300

    Update Lambda to read QueryParameters and allow API Gateway to Invoke Lambda

--- Node/cardalog_data_reader_function.js
+++ Node/cardalog_data_reader_function.js
@@ -2,34 +2,39 @@ const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
-    try {
-        // Parameters to query DynamoDB table
-        const params = {
-            TableName: 'cardalog-card-data',
-            Key: {
-                //primaryKeyCardalog: 'testorino'
-                primaryKeyCardalog: event.itemName
-            }
-        };
 
-        // Query DynamoDB table
-        const data = await docClient.get(params).promise();
+    const queryParams = event.queryStringParameters;
 
-        // Log the retrieved data
-        console.log('Retrieved data from DynamoDB:', data);
+    if (queryParams && Object.keys(queryParams).length > 0) {
 
-        // Return the retrieved item
-        return {
-            statusCode: 200,
-            body: JSON.stringify(data.Item)
-        };
-    } catch (error) {
-        console.error('Error retrieving data from DynamoDB:', error);
+        try {
+            // Parameters to query DynamoDB table
+            const params = {
+                TableName: 'cardalog-card-data',
+                Key: {
+                    primaryKeyCardalog: queryParams.itemName
+                }
+            };
 
-        // Return an error response
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: 'Internal Server Error' })
-        };
+            // Query DynamoDB table
+            const data = await docClient.get(params).promise();
+
+            // Log the retrieved data
+            console.log('Retrieved data from DynamoDB:', data);
+
+            // Return the retrieved item
+            return {
+                statusCode: 200,
+                body: JSON.stringify(data.Item)
+            };
+        } catch (error) {
+            console.error('Error retrieving data from DynamoDB:', error);
+
+            // Return an error response
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ error: 'Internal Server Error' })
+            };
+        }
     }
 };
